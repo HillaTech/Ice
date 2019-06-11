38b41065f13fe975f5f400bb10129ce395b2b862
mahdi <mahdi@mahdi.local>
2019-06-11 18:52:30 +0430

    congif intro animation and ui
    connect intro to login

--- hillapay/AppDelegate.swift
+++ hillapay/AppDelegate.swift
@@ -17,9 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        let Home = IntroViewController()
+        let Intro = IntroViewController()
         
-        window!.rootViewController = Home
+        window!.rootViewController = Intro
         window!.makeKeyAndVisible()
         
         return true
--- hillapay/Source/Views/Intro/IntroViewController.swift
+++ hillapay/Source/Views/Intro/IntroViewController.swift
@@ -18,7 +18,15 @@ class IntroViewController: UIPageViewController {
                 self.newColoredViewController(imageName: "slides_3"),
                 self.newColoredViewController(imageName: "slides_4")]
     }()
-
+    
+    init() {
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,18 +34,12 @@ class IntroViewController: UIPageViewController {
         delegate = self
         
         if let firstViewController = orderedViewControllers.first {
-            setViewControllers([firstViewController],
-                               direction: .forward,
-                               animated: true,
-                               completion: nil)
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
         }
         
-        configurePageControl()
+        configPageControl()
+        configButton()
         
-        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
-        button.backgroundColor = UIColor.white
-        
-        self.view.addSubview(button)
     }
     
     private func newColoredViewController(imageName: String) -> UIViewController {
@@ -47,15 +49,35 @@ class IntroViewController: UIPageViewController {
         return vc
     }
     
-    func configurePageControl() {
-        // The total number of pages that are available is based on how many available colors we have.
-        pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - 50,width: UIScreen.main.bounds.width,height: 50))
+    func configPageControl() {
+        
+        pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - 70,width: UIScreen.main.bounds.width,height: 50))
         self.pageControl.numberOfPages = orderedViewControllers.count
         self.pageControl.currentPage = 0
-        self.pageControl.tintColor = UIColor.black
-        self.pageControl.pageIndicatorTintColor = UIColor.white
-        self.pageControl.currentPageIndicatorTintColor = UIColor.black
+        self.pageControl.tintColor = UIColor.white
+        self.pageControl.pageIndicatorTintColor = UIColor.lightGray
+        self.pageControl.currentPageIndicatorTintColor = UIColor.white
         self.view.addSubview(pageControl)
+    }
+    
+    func configButton() {
+        
+        let XPosition = self.view.bounds.maxX - 100
+        let YPosition = self.view.bounds.maxY - 70
+        let button = CButton(frame: CGRect(x: XPosition, y: YPosition, width: 80, height: 50))
+        button.backgroundColor = UIColor.PRIMARY_5
+        button.setTitle("ورود", for: .normal)
+        button.cornerRadius = 25
+        button.titleLabel?.font = FontHandler.getRegular(size: 16)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        self.view.addSubview(button)
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        
+        let Login = LoginStep1ViewController()
+        Login.modalTransitionStyle = .crossDissolve
+        self.present(Login, animated: true, completion: nil)
     }
 
 }
@@ -63,6 +85,14 @@ class IntroViewController: UIPageViewController {
 // MARK: UIPageViewControllerDataSource
 
 extension IntroViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    override var transitionStyle: UIPageViewController.TransitionStyle {
+        return .scroll
+    }
+    
+    override var navigationOrientation: UIPageViewController.NavigationOrientation {
+        return .horizontal
+    }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         
